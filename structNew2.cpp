346502e46a1e861d42140a422def11b3b5d7e83b
upi1682 <upi538287@gmail.com>
2026-04-06 18:33:53 +0700

    mengisi data

--- structNew2.cpp
+++ structNew2.cpp
@@ -7,5 +7,24 @@ struct detailAlamat {
 
 };
 
+struct Orang {
+    string nama;
+    detailAlamat alamat;
+    int umur;
+};
 
+int main(){
+    Orang mhs;
+    //mengisi data
+    cout << "Mengisi data" << endl;
+    cout << "Nama: ";
+    cin >> mhs.nama;
+    cout << "Alamat: ";
+    cin >> mhs.alamat.desa;
+    cout << "Kota: ";
+    cin >> mhs.alamat.kota;
+    cout << "Umur: ";
+    cin >> mhs.umur;
 
+     
+}
