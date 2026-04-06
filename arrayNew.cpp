e153a279b7cfb4b94ea51381be16492f7e03567f
upi1682 <upi538287@gmail.com>
2026-04-06 17:33:02 +0700

    deklarasi dengan inisialisasi

--- arrayNew.cpp
+++ arrayNew.cpp
@@ -1,2 +1,21 @@
 #include <iostream>
 using namespace std;
+
+
+int main() {
+    // deklarasi dengan inisialisasi
+    int aku[5] = {10,20,30,40,50};
+    
+   
+  ;
+    
+
+ 
+
+  
+    }
+
+    cout << endl;
+      
+
+}
